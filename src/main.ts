56987932dd9e2451755da8fe9997c7b556bca8da
HM <hieumdd@gmail.com>
2022-10-07 23:20:14 +0700

    feat: Remove prefix

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,6 @@ const bootstrap = async () => {
         logger: new CloudLoggingLogger(),
     });
     app.use(cookieParser());
-    app.setGlobalPrefix('api');
 
     const config = new DocumentBuilder()
         .setTitle('Real Home Data')
