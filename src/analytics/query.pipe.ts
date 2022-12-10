57ca14fa590858a18584510602a8cb327ce2a2da
HM <hieumdd@gmail.com>
2022-12-10 23:53:57 +0700

    fix: Fix zipCode parsing

--- src/analytics/query.pipe.ts
+++ src/analytics/query.pipe.ts
@@ -4,17 +4,19 @@ import { isString } from 'lodash';
 type QueryArray = {
     county?: string | string[];
     city?: string | string[];
+    zipCode? : string | string[];
 };
 
 @Injectable()
 export class ParseQueryArray<T extends QueryArray> implements PipeTransform {
     transform(value: T): T {
-        const { county, city } = value;
+        const { county, city, zipCode } = value;
 
         return {
             ...value,
             county: isString(county) ? [county] : value.county,
             city: isString(city) ? [city] : value.city,
+            zipCode: isString(zipCode) ? [zipCode] : value.zipCode,
         };
     }
 }
