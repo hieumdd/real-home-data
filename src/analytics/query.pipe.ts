22f5095120feadcc02e940f9187235b06320c4e4
HM <hieumdd@gmail.com>
2023-01-02 23:47:13 +0700

    hotfix: Hotfix postal_code parsing

--- src/analytics/query.pipe.ts
+++ src/analytics/query.pipe.ts
@@ -4,19 +4,19 @@ import { isString } from 'lodash';
 type QueryArray = {
     county?: string | string[];
     city?: string | string[];
-    zipCode? : string | string[];
+    postal_code? : string | string[];
 };
 
 @Injectable()
 export class ParseQueryArray<T extends QueryArray> implements PipeTransform {
     transform(value: T): T {
-        const { county, city, zipCode } = value;
+        const { county, city, postal_code } = value;
 
         return {
             ...value,
             county: isString(county) ? [county] : value.county,
             city: isString(city) ? [city] : value.city,
-            zipCode: isString(zipCode) ? [zipCode] : value.zipCode,
+            postal_code: isString(postal_code) ? [postal_code] : value.postal_code,
         };
     }
 }
